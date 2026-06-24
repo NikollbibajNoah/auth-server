10352df453df48feb92c2b52ed317f4e8e71f834
NikollbibajNoah <nikollbibaj.noah@gmail.com>
2026-06-24 11:20:19 +0200

    WIP: finishing testing authProvider

--- tests/unit/authProvider.test.ts
+++ tests/unit/authProvider.test.ts
@@ -1,14 +1,38 @@
-
 import { prismaMock } from "../helpers/singleton";
-import { register } from "../../src/service/authProvider";
+import { login, logout, refreshToken, register } from "../../src/service/authProvider";
+import { getUserPayload } from "../../src/lib/utils";
+import { sign, verify } from 'jsonwebtoken';
 
 import bcrypt from "bcrypt";
 
 jest.mock("bcrypt");
+jest.mock("../../src/lib/utils", () => ({
+    getUserPayload: jest.fn(),
+}));
+jest.mock("jsonwebtoken", () => ({
+    sign: jest.fn(),
+    verify: jest.fn(),
+}));
 
 const mockBcryptHash = bcrypt.hash as jest.Mock;
+const mockBcryptCompare = bcrypt.compare as jest.Mock;
+
+const mockGetUserPayload = getUserPayload as jest.Mock;
+const mockSign = sign as jest.Mock;
+const mockVerify = verify as jest.Mock;
 
 describe("AuthProvider", () => {
+    const existingUser = {
+        id: "123",
+        username: "existinguser",
+        email: "existinguser@example.com",
+        password: "Hashedpassword1",
+        roleId: "role-1",
+        createdAt: new Date(),
+        refreshToken: null,
+    };
+
+    // --- POST /register -----------------------------------------------
 
     describe("authProvider - register", () => {
         
@@ -86,16 +110,6 @@ describe("AuthProvider", () => {
         
         describe("conflict", () => {
             it("should fail if user already exists", async () => {
-                const existingUser = {
-                    id: "123",
-                    username: "existinguser",
-                    email: "existinguser@example.com",
-                    password: "Hashedpassword1",
-                    roleId: "role-1",
-                    createdAt: new Date(),
-                    refreshToken: null,
-                };
-
                 prismaMock.user.findFirst.mockResolvedValue(existingUser);
 
                 await expect(register({
@@ -140,4 +154,127 @@ describe("AuthProvider", () => {
             });
         });
     });
+
+    // --- POST /login -----------------------------------------------
+
+    describe("authProvider - login", () => {
+        describe("validation", () => {
+            it("should fail if email is invalid", async () => {
+                await expect(login({
+                    email: "invalid-email",
+                    password: "Password1",
+                })).rejects.toThrow("Invalid email address");
+            });
+
+            it("should fail if password is missing", async () => {
+                await expect(login({
+                    email: "testuser@example.com",
+                    password: "",
+                })).rejects.toThrow("Password is required");
+            });
+        });
+
+        describe("authentication", () => {
+            it("should fail if user does not exist", async () => {
+                prismaMock.user.findFirst.mockResolvedValue(null);
+
+                await expect(login({
+                    email: "newuser@example.com",
+                    password: "Password1",
+                })).rejects.toThrow("Invalid email or password");
+            });
+
+            it("should fail if password is incorrect", async () => {
+                prismaMock.user.findFirst.mockResolvedValue(existingUser);
+                mockBcryptCompare.mockResolvedValue(false);
+
+                await expect(login({
+                    email: "existinguser@example.com",
+                    password: "WrongPassword1",
+                })).rejects.toThrow("Invalid email or password");
+            });
+        });
+
+        describe("success", () => {
+            it("should return accessToken and refreshToken on successful login", async () => {
+                prismaMock.user.findFirst.mockResolvedValue(existingUser);
+                mockBcryptCompare.mockResolvedValue(true);
+                mockGetUserPayload.mockResolvedValue({ email: existingUser.email, role: "user" });
+                mockSign
+                    .mockReturnValueOnce("mockedAccessToken")
+                    .mockReturnValueOnce("mockedRefreshToken");
+
+                prismaMock.user.update.mockResolvedValue(existingUser);
+
+                const result = await login({
+                    email: existingUser.email,
+                    password: "Password1",
+                });
+
+                expect(result).toEqual({
+                    statusCode: 200,
+                    message: "Login successful",
+                    accessToken: "mockedAccessToken",
+                    refreshToken: "mockedRefreshToken",
+                });
+            });
+        });
+    });
+
+    // --- POST /logout -----------------------------------------------
+
+    describe("authProvider - logout", () => {
+        it("should successfully logout a user", async () => {
+            prismaMock.user.update.mockResolvedValue({ ...existingUser, refreshToken: "refreshToken" });
+
+            const result = await logout(existingUser.email);
+
+            expect(result).toEqual({
+                statusCode: 200,
+                message: "Logout successful",
+            })
+        });
+    });
+
+    // --- POST /refreshToken -----------------------------------------------
+
+    describe("authProvider - refreshToken", () => {
+        describe("validation", () => {
+            it("should fail if token is missing", async () => {
+                await expect(refreshToken("")).rejects.toThrow("Refresh token is required");
+            });
+        });
+
+        describe("token reuse", () => {
+            it("should fail if token does not exist in DB", async () => {
+                mockVerify.mockReturnValue({ email: "existinguser@example.com" });
+                prismaMock.user.findUnique.mockResolvedValue(null);
+                prismaMock.user.updateMany.mockResolvedValue({ count: 1});
+
+                await expect(refreshToken("someInvalidToken"))
+                    .rejects.toThrow("Invalid refresh token");
+            });
+        });
+
+        describe("success", () => {
+            it("should return new accessToken and refreshToken", async () => {
+                mockVerify.mockReturnValue({ email: "existinguser@example.com" });
+                prismaMock.user.findUnique.mockResolvedValue({ ...existingUser, refreshToken: "validRefreshToken" });
+                mockGetUserPayload.mockResolvedValue({ email: existingUser.email, role: "user" });
+                mockSign
+                    .mockReturnValueOnce("mockedAccessToken")
+                    .mockReturnValueOnce("mockedRefreshToken");
+                prismaMock.user.update.mockResolvedValue(existingUser);
+
+                const result = await refreshToken("validRefreshToken");
+
+                expect(result).toEqual({
+                    statusCode: 200,
+                    message: "Token refreshed successfully",
+                    accessToken: "mockedAccessToken",
+                    refreshToken: "mockedRefreshToken",
+                });
+            });
+        });
+    })
 });
